ed7c257e08757b01cbd66fe950d0680e553b3c26
Joao Passos <joao.passos@invillia.com>
2023-10-15 21:47:27 -0300

    ajuste vercel

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: 'http://localhost:4200'
-}));
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', routes);
-app.listen(port, () => {
-  console.log(`Servidor está rodando na porta ${port}`);
-});
+
+module.exports = app;
